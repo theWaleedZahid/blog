e0731c0728ecd05b6ce02db11044cec550601d5d
Waleed Zahid <waleed.zahid654@gmail.com>
2016-06-26 11:41:12 +0000

    Fix Comment Show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,9 @@
 class CommentsController < ApplicationController
-    	def create
+ 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:comment))
-		#@comment.user_id = current_user.id if current_user
-		@comment.save
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 
-		if @comment.save
-			redirect_to post_path(@post)
-		else
-			render 'new'
-		end
+		redirect_to post_path(@post)
 	end
 
 	def destroy
@@ -18,18 +12,5 @@ class CommentsController < ApplicationController
 		@comment.destroy
 
 		redirect_to post_path(@post)
-	end
-	def upvote
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
-		@comment.liked_by current_user
-		redirect_to @post
-	end
-
-	def downvote
-	  @post = Post.find(params[:post_id])
-	  @comment = @post.comments.find(params[:id])
-      @comment.downvote_from current_user
-      redirect_to @post
 	end
 end
