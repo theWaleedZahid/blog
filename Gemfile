8e9182e7307090e561429f0b3af067f3fe5b9d90
Waleed Zahid <waleed.zahid654@gmail.com>
2016-06-27 04:31:26 +0000

    Setting Up Admin Panel

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem 'acts_as_votable'
-gem 'impressionist'
+gem 'rails_admin'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160626115504) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
