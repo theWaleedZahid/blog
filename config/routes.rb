57e044739eb2a2d728384deca6e2252388f4ce1d
Waleed Zahid <waleed.zahid654@gmail.com>
2016-06-27 12:59:55 +0000

    Add Terms and Condition Page

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get '/about', to: 'pages#about'
+  get '/terms', to: 'pages#terms'
 end
--- db/migrate/20160624120809_devise_create_users.rb
+++ db/migrate/20160624120809_devise_create_users.rb
@@ -3,6 +3,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.boolean :admin
       ## Database authenticatable
+      t.string :user_name,
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
