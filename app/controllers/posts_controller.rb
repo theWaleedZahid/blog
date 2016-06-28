96f88ad8957860e3e711644380b941a80352332b
Waleed Zahid <waleed.zahid654@gmail.com>
2016-06-28 11:49:28 +0000

    Added Pagination and styled it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     def index
         @posts=Post.all.order("created_at DESC")
+        @posts=Post.paginate(:page => params[:page], :per_page => 3)
     end
     def new
         @post = current_user.posts.build
