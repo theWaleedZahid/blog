72bbad7768898959be8efa98d196e3091ff530fb
Waleed Zahid <waleed.zahid654@gmail.com>
2016-06-28 12:05:49 +0000

    Added 10 posts per page - pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     def index
         @posts=Post.all.order("created_at DESC")
-        @posts=Post.paginate(:page => params[:page], :per_page => 3)
+        @posts=Post.paginate(:page => params[:page], :per_page => 10)
     end
     def new
         @post = current_user.posts.build
