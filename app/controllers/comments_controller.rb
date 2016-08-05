c98821340d8c896542089a5b3662028c7b5ffa83
Waleed Zahid <waleed.zahid654@gmail.com>
2016-08-05 11:01:58 +0500

    Added comment deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
 		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 		redirect_to post_path(@post)
 	end
-
 	def destroy
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701161335) do
+ActiveRecord::Schema.define(version: 20160805052903) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160701161335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "email"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
@@ -89,12 +90,10 @@ ActiveRecord::Schema.define(version: 20160701161335) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "fullname"
-    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "votes", force: :cascade do |t|
