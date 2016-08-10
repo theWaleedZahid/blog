2e1ddb758c1d77a567de886520f6237cd4df5d89
Waleed Zahid <waleed.zahid654@gmail.com>
2016-08-10 16:01:59 +0000

    Fixed Bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
     before_action :find_post, only: [:show,:edit,:update,:destroy]
     before_action :authenticate_user!, except: [:index, :show]
-    def delete_image
-    @post.image.destroy 
-    @post.image.clear 
-    end
     def index
         @posts=Post.all.order("created_at DESC")
+        if params[:search]
+            @posts = Post.where(["title LIKE ?","%#{params[:search]}%"])
+        else
+            @posts = Post.all.order('created_at DESC')
+        end
     end
     def new
         @post = current_user.posts.build
